2e16d3a2aadd306c2035d70fed44705b1875e7ab
Iván Andres Yagual Méndez <ivanandres.yagual@edu.upct.es>
2024-10-16 19:54:44 +0200

    Cambios menores

--- Algoritmo Voraz/voraz algorithm.py
+++ Algoritmo Voraz/voraz algorithm.py
@@ -23,7 +23,7 @@ def read_input_from_file(filename: str) -> list[dict]:
     return cases # list of dictionaries
 
 
-def voraz(case: dict) -> list:
+def voraz(case: dict) -> tuple:
     sol = []
     budget = case['budget'] 
     number_garments = case['number_garments'] 
@@ -31,10 +31,9 @@ def voraz(case: dict) -> list:
     candidates = []
 
     for garment, models in enumerate(garments):
-        for price in models:#REcordar error no gestiona bien el numero de prende
-            print(garment)
-            candidates.append((price, garment))  # (price, garment_type)
-    
+        for price in models:
+            candidates.append((price, garment + 1))  # (price, garment_type)
+
     candidates.sort(reverse=True, key=lambda price: price[0])
 
     while len(candidates) > 0 and not solution(sol, number_garments):
@@ -42,20 +41,24 @@ def voraz(case: dict) -> list:
         if factible(sol, candidate, budget):
             insert(sol, candidate)
 
+
     if not solution(sol, number_garments):
         return "No se puede encontrar solución"
     
-    return sol, sum(price for price, _ in sol) #Solucion y 
+    return sol, sum(price for price, _ in sol), budget #sol:  
 
 
-def select(candidates:list) -> int:
-    return candidates[0]
+
 
 
 def factible(sol: list, candidate: tuple, budget: int) -> bool: 
     total_price = sum(price for price, _ in sol)  #!!!!!NO SE SI 'total_price' ES UN NOMBRE ADECUADO!!!
 
-    return (candidate not in sol) and (total_price <= budget)
+    porcentaje_ocupa_dinero_gastado = total_price / budget  * 100
+    porcentaje_faltante = 100 - porcentaje_ocupa_dinero_gastado
+    porcentaje_candidato = candidate[0] / budget * 100
+
+    return porcentaje_candidato < porcentaje_faltante / 2
 
 
 def solution(sol: list, number_garments: int) -> bool:
@@ -63,21 +66,22 @@ def solution(sol: list, number_garments: int) -> bool:
 
 
 def insert(sol: list, candidate: tuple):
-    sol.append(candidate)
+    garments_list = [garment for _, garment in sol] # list of garment type.
+    if candidate[1] not in garments_list: #If there is one type garment in the list, no more will be added
+        sol.append(candidate)
 
 
 
 #Read file
 #Rute Iván
-filename = '/home/ivan/Universidad/2º Curso/Analisis y diseños de algoritmos/PRACTICAS/Algoritmo Voraz/tests/T1/901a.in'
+filename = '/home/ivan/Universidad/2º Curso/Analisis y diseños de algoritmos/PRACTICAS/Algoritmo Voraz/tests/T1/901a (Copiar).in'
 
 #Rute Katerine (no se si se escribe asi (*^_^*)
 #filename = '/home/ivan/Universidad/2º Curso/Analisis y diseños de algoritmos/ADA_boletin_P1/enunciados/04 - boda/tests/T1/901a.in'
 
 cases = read_input_from_file(filename) # dictionarys list [{M:Budget, C:number garments, garments: models lists}]
 
-
 for case in cases:
     #the case are set of garments
     result = voraz(case)  # Result: set of garments opt.
-    print(result)
+    print(result[1], result[2])
