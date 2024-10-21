09a92be12030a02088f32c4c8979fa93ce9a4a5c
Iván Andres Yagual Méndez <ivanandres.yagual@edu.upct.es>
2024-10-21 18:08:53 +0200

    Versión Mitad-Mínimo

--- Algoritmo Voraz/voraz algorithm.py
+++ Algoritmo Voraz/voraz algorithm.py
@@ -5,15 +5,12 @@ def read_input_from_file(filename: str) -> list[dict]:
         Number_cases = int(file.readline().strip())
 
         for _ in range(Number_cases):
-
             line = file.readline().strip().split()
             budget, number_garments = [int(x) for x in line] # M - budget, C - number of garments
-
             garments = []
 
             for _ in range(number_garments):
                 line = list(map(int, file.readline().strip().split()))
-                K = line[0] # number of models for each particular garment ??????????
                 prices = line[1:]
                 garments.append(prices)  # matrix of garments
 
@@ -30,58 +27,77 @@ def voraz(case: dict) -> tuple:
     garments = case['garments']
     candidates = []
 
+   # for garment, models in enumerate(garments):
+    #    for price in models:
+     #       candidates.append((price, garment + 1))  # (price, garment_type)
     for garment, models in enumerate(garments):
-        for price in models:
-            candidates.append((price, garment + 1))  # (price, garment_type)
+        candidates.append((models, garment + 1))  # (price, garment_type)
 
-    candidates.sort(reverse=True, key=lambda price: price[0])
-
+    cont = 0
+    type_garment_added = []
     while len(candidates) > 0 and not solution(sol, number_garments):
         candidate = candidates.pop(0) # Delete the first candidate
-        if factible(sol, candidate, budget):
-            insert(sol, candidate)
+        if candidate[1] not in type_garment_added:
+            if cont % 2 == 0:
+                insert(sol, candidate, budget)
+            else:
+                prueba(sol,candidate, budget)
+            type_garment_added.append(candidate[1])
+        cont = cont + 1
 
-
-    if not solution(sol, number_garments):
-        return "No se puede encontrar solución"
     
-    return sol, sum(price for price, _ in sol), budget #sol:  
+#    if not solution(sol, number_garments):
+ #       print("No se puede encontrar solución")
+    
+
+    return sol, budget #sol:  
 
 
-
-
-
-def factible(sol: list, candidate: tuple, budget: int) -> bool: 
-    total_price = sum(price for price, _ in sol)  #!!!!!NO SE SI 'total_price' ES UN NOMBRE ADECUADO!!!
-
-    porcentaje_ocupa_dinero_gastado = total_price / budget  * 100
-    porcentaje_faltante = 100 - porcentaje_ocupa_dinero_gastado
-    porcentaje_candidato = candidate[0] / budget * 100
-
-    return porcentaje_candidato < porcentaje_faltante / 2
 
 
 def solution(sol: list, number_garments: int) -> bool:
     return len(sol) == number_garments
 
 
-def insert(sol: list, candidate: tuple):
-    garments_list = [garment for _, garment in sol] # list of garment type.
-    if candidate[1] not in garments_list: #If there is one type garment in the list, no more will be added
-        sol.append(candidate)
+def prueba(sol,candidate, budget:int):
+    price_min = min(candidate[0])
+    sol.append((price_min, candidate[1]))
 
+#center value
+def insert(sol: list, candidate: tuple, budget:int):
+    l = candidate[0]
+    l.sort()
+    medio = len(l) // 2
+    price_min = l[medio]
+    price_total = 0
+    for price, _ in sol:
+        price_total = price + price_total 
+    if price_total < budget/len(l):
+        sol.append((price_min, candidate[1]))
+    else:
+        prueba
 
 
 #Read file
 #Rute Iván
-filename = '/home/ivan/Universidad/2º Curso/Analisis y diseños de algoritmos/PRACTICAS/Algoritmo Voraz/tests/T1/901a (Copiar).in'
+filename = '/home/ivan/Universidad/2º Curso/Analisis y diseños de algoritmos/PRACTICAS/Algoritmo Voraz/tests/T1/901a.in'
+filename_out = '/home/ivan/Universidad/2º Curso/Analisis y diseños de algoritmos/PRACTICAS/Algoritmo Voraz/tests/T1/901a (Copiar).out'
 
 #Rute Katerine (no se si se escribe asi (*^_^*)
 #filename = '/home/ivan/Universidad/2º Curso/Analisis y diseños de algoritmos/ADA_boletin_P1/enunciados/04 - boda/tests/T1/901a.in'
 
 cases = read_input_from_file(filename) # dictionarys list [{M:Budget, C:number garments, garments: models lists}]
-
 for case in cases:
     #the case are set of garments
-    result = voraz(case)  # Result: set of garments opt.
-    print(result[1], result[2])
+    sol, budget = voraz(case)  # Result: set of garments opt.
+    if len(sol) > 0:
+        price_total = 0
+        print("")
+        for price, gurment in sol:
+            price_total = price_total + price
+            #print(f"La prenda {gurment} cuesta {price} presupuesto {budget}")
+        if price_total < budget:    
+            print(f"Se gasto un total de: {price_total} con presupuesto de {budget}")
+        else:
+            print(f"Se gasto un total de: {price_total} con presupuesto de {budget} FALLO")
+    
