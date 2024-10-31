da9635d6c8d8db16f021d4b0ae5cf8e4166b53e6
Iván Andres Yagual Méndez <ivanandres.yagual@edu.upct.es>
2024-10-31 16:22:48 +0100

    Algorithm_weight

--- Algoritmo Voraz/voraz algorithm_inter_sort.py
+++ Algoritmo Voraz/voraz algorithm_inter_sort.py
@@ -24,21 +24,16 @@ def read_input_from_file(filename):
     return cases # list of dictionaries
 
 
-filename = '901a.in'
+filename = 'tests/T1/901a.in'
 cases = read_input_from_file(filename)
 
 def voraz(budget, garments, gar_number):
     s = []  # Selected items list
     current_sum = 0 # Track the current sum of selected prices
+    weight_garments = add_weights(garments)
 
-    sorted_garments = sorted(garments, key=lambda x:
-    (1 / 8 * (max(x) - min(x)) +  # prioritize small range
-     1 / 8 * max(x) +  # prioritize biggest maximum
-     3 / 4 * -len(x)),  # prioritize shortest list
-                             reverse=True)
-
-    for garment in sorted_garments:
-
+    for _ in range(len(weight_garments)):
+        garment = better_weight(weight_garments)
         max_feasible_value = None
 
         actual_maximum_prize = (budget - current_sum) / (gar_number - len(s))
@@ -57,6 +52,26 @@ def voraz(budget, garments, gar_number):
         return "No se puede encontrar solución"
 
     return s, sum(s)
+
+
+
+
+def add_weights(var_list):
+    list_tuple = []
+    for value in var_list:
+        wights = 1/8 * max(value) - min(value) + 1/8 * max(value) + 3/4 * len(value) 
+        list_tuple.append((value, wights)) 
+    return list_tuple
+            
+    
+def better_weight(weight_garments):
+    max_weight = 0
+    for value, weight in weight_garments:
+        if max_weight < weight:
+            max_weight = weight
+            garment = value
+    return garment 
+
 
 def feasible(model_price, garment, actual_maximum_prize, current_sum, budget, max_feasible_value):
 
